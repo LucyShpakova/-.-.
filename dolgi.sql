75874d6e57fe3350736cd5a59ccb8ab93434b2f5
LucyShpakova <160004371+LucyShpakova@users.noreply.github.com>
2024-02-14 11:39:42 +0300

    Add files

---
+++ dolgi.sql
@@ -0,0 +1,88 @@
+﻿--- Сумма остаточного долга (ИТОГО ОДНОЙ СУММОЙ)
+
+SELECT
+	  ipeopleid
+	, fio
+	, adr
+	, vidz
+	, array_to_string(array_accum(distinct cast(number as text)), ', ') as z
+	, date_part('year',dtstart) as god
+	, SUM(dolg1) as dolg
+
+FROM
+
+ (
+  SELECT
+	  p.ipeopleid
+	, p.dtbirthday as dtr
+	, d.vcnumber as number
+	, (case when d.istpdemid=186 then 'kaprem' when d.istpdemid = 11 then 'subs' else 'lgoty' end) as vidz
+	,  de.idebtid
+	, de.dtstart
+	, de.vcname
+	, de.idemandid
+	, de.decsum
+	, de.vccomment
+	, de.vcnumber
+	, de.sumuder
+	, de.sumpogas
+	, dolg1
+	, p.vcsurname||' '||p.vcname||' '||p.vcfathername as fio
+	, f_getaddressidtoaddress2(d.iaddressid) as adr
+
+  FROM demand as d
+	INNER JOIN people as p using (ipeopleid)
+	JOIN
+	(      -- признак учета из заявления
+	 SELECT d3.idemandid
+	 FROM demval d3
+	) AS d4 USING (idemandid)
+	INNER JOIN 
+        (      -- непогашенные долги 
+         SELECT
+		  de.idebtid
+		, de.dtstart
+		, de.vcname
+		, de.idemandid
+		, de.decsum
+		, de.vccomment
+		, de.vcnumber
+		, ud.sumuder
+		, po.sumpogas
+		, de.decsum-(case when (ud.sumuder IS NULL) then 0.00 else ud.sumuder end)-(case when (po.sumpogas IS NULL) then 0.00 else po.sumpogas end) as dolg1
+        FROM debt de
+		LEFT JOIN 
+		(
+		 SELECT idebtid, SUM(decsum) as sumuder
+		 FROM lkdebtfix
+		 GROUP BY idebtid
+		) AS ud USING (idebtid)
+		LEFT JOIN 
+		(
+		 SELECT idebtid, SUM(cast(decsum  as numeric(10,2))) as sumpogas
+		 FROM payingoff
+		 GROUP BY idebtid
+		) AS po USING (idebtid)
+   
+        WHERE 
+         (decsum <> 0)
+       ) AS de USING (idemandid)
+  WHERE ((d.istpdemid IN (186,159,160,190,191,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,210,211,212,220,11) AND (d.dtstart >= '01.11.2011')) )  -- 220 - проезд рег.льгот
+       --or d.istpdemid IN (24,25,26))
+       AND  p.ipeopleid = '610038001000071169'   -- ВЫБРАТЬ ЛЬГОТНИКА!!!!!!
+  ORDER BY 2,3,4
+ ) 
+AS dlg
+
+WHERE dolg1<>0
+ 
+
+GROUP BY  ipeopleid
+	, fio
+	, adr
+	, vidz
+	, god
+	--,inormactid
+	--,zakon
+	--, number
+ORDER by fio, god, z
